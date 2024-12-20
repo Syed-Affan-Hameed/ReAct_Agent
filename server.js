a5b5817bc083858c67715510fa37b1503747bbd1
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-12-20 15:40:01 +0530

    Manual Function Calling

--- server.js
+++ server.js
@@ -1,109 +1,95 @@
 import OpenAI from "openai"
-import { getCurrentWeather, getLocation } from "./tools.js"
+import { getCurrentWeather, getLocation, tools } from "./tools.js"
 import dotenv from "dotenv"
+
 dotenv.config();
+
 export const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 })
 
-/* 
-Key learning point here
-->To build ReAct Agent
-->How to call a function dynamically using string based object-property access capability of javascript.
- */
 const availableFunctions = {
     getCurrentWeather,
     getLocation
 }
 
-/**
- * Goal - build an agent that can answer any questions that might require knowledge about my current location and the current weather at my location.
- */
-
-const systemPrompt = `
-You cycle through Thought, Action, PAUSE, Observation. At the end of the loop you output a final Answer. Your final answer should be highly specific to the observations you have from running
-the actions.
-1. Thought: Describe your thoughts about the question you have been asked.
-2. Action: run one of the actions available to you - then return PAUSE.
-3. PAUSE
-4. Observation: will be the result of running those actions.
-
-Available actions:
-- getCurrentWeather: 
-    E.g. getCurrentWeather: Salt Lake City
-    Returns the current weather of the location specified.
-- getLocation:
-    E.g. getLocation: null
-    Returns user's location details. No arguments needed.
-
-Example session:
-Question: Please give me some ideas for activities to do this afternoon.
-Thought: I should look up the user's location so I can give location-specific activity ideas.
-Action: getLocation: null
-PAUSE
-
-You will be called again with something like this:
-Observation: "New York City, NY"
-
-Then you loop again:
-Thought: To get even more specific activity ideas, I should get the current weather at the user's location.
-Action: getCurrentWeather: New York City
-PAUSE
-
-You'll then be called again with something like this:
-Observation: { location: "New York City, NY", forecast: ["sunny"] }
-
-You then output:
-Answer: <Suggested activities based on sunny weather that are highly specific to New York City and surrounding areas.>
-`
-
 async function agent(query) {
     const messages = [
-        { role: "system", content: systemPrompt },
+        { role: "system", content: "You are a helpful AI agent. Give highly specific answers based on the information you're provided. Prefer to gather information with the tools provided to you rather than giving basic, generic answers." },
         { role: "user", content: query }
     ]
-    
-    const MAX_ITERATIONS = 5
-    const actionRegex = /^Action: (\w+): (.*)$/
-    
+
+    const MAX_ITERATIONS = 3
+
     for (let i = 0; i < MAX_ITERATIONS; i++) {
         console.log(`Iteration #${i + 1}`)
         const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages
+            model: "gpt-3.5-turbo-1106",
+            messages,
+            tools
         })
-
-        const responseText = response.choices[0].message.content
-        console.log(responseText)
-        messages.push({ role: "assistant", content: responseText })
-        const responseLines = responseText.split("\n")
-
-        const foundActionStr = responseLines.find(str => actionRegex.test(str))
-        
-        if (foundActionStr) {
-            const actions = actionRegex["exec"](foundActionStr)
-            const [_, action, actionArg] = actions
+        if(response.choices[0].finish_reason === "stop"){
+            console.log(response.choices[0].finish_reason);
+            console.log(response.choices[0].message.content);
+            console.log("Agent: Task Complete.");
             
-            if (!availableFunctions.hasOwnProperty(action)) {
-                throw new Error(`Unknown action: ${action}: ${actionArg}`)
+            return response.choices[0].message.content;
+        }
+        else if(response.choices[0].finish_reason ==="tool_calls")
+        {
+            const toolCallMessage = response.choices[0].message;
+            console.log("Tool Calls Array",toolCallMessage.tool_calls);
+            const toolCallsArray = response.choices[0].message.tool_calls;
+            messages.push(toolCallMessage);
+
+            for(const toolCall of toolCallsArray){
+
+                const selectedFunction = toolCall.function.name;
+                const selectedFunctionArguments = JSON.parse(toolCall.function.arguments);
+                //We are calling the function here using the string access property of an object in javascript
+                const functionResponseValue = await availableFunctions[selectedFunction](selectedFunctionArguments);
+                console.log(`Name of the function getting called ${selectedFunction} and Function Respone ${functionResponseValue}`);
+                messages.push({
+                    tool_call_id:toolCall.id,
+                    role:"tool",
+                    name:selectedFunction,
+                    content:functionResponseValue
+                });
+
             }
-            console.log(`Calling function ${action} with argument ${actionArg}`)
-            const observation = await availableFunctions[action](actionArg)
-            messages.push({ role: "assistant", content: `Observation: ${observation}` })
-        } else {
-            console.log("Agent finished with task")
-            return responseText
         }
     }
-    
 }
 
-console.log(await agent("What are some activity ideas that I can do this afternoon based on my location and weather?"))
+await agent("what is the weather in my location today?")
 
 /**
-Answer: Based on the current snowy weather in New York City, here are some activity ideas for this afternoon: 1. Build a snowman in Central Park. 2. Go ice skating at Rockefeller Center. 3. Have a hot chocolate tasting at a cozy café. 4. Visit a museum or art gallery. 5. Stay indoors and watch a movie marathon. Remember to dress warmly and stay safe while enjoying these activities!
-/index.html
+{
+    "index": 0,
+    "message": {
+        "role": "assistant",
+        "content": "As an AI, I don't have feelings, but I'm here to assist you with any questions or tasks you have. How can I help you today?"
+    },
+    "finish_reason": "stop"
+}
 
 
-
+{
+    "index": 0,
+    "message": {
+        "role": "assistant",
+        "content": null,
+        "tool_calls": [
+            {
+                "id": "call_SDhXnJbvxSWwy1m1R1J43EmQ",
+                "type": "function",
+                "function": {
+                    "name": "getLocation",
+                    "arguments": "{}"
+                }
+            }
+        ]
+    },
+    "finish_reason": "tool_calls"
+}
  */
--- tools.js
+++ tools.js
@@ -1,12 +1,62 @@
-export const getCurrentWeather = async () => {
+export const getCurrentWeather = async ({location}) => {
+
   return JSON.stringify({
+    location:location,
     type: "snowy",
     temperature: 72.5,
   });
 };
 
 export const getLocation = async () => {
+  try {
+    const locationFromAPIResponse = await fetch('https://ipapi.co/json');
+    
+    if (!locationFromAPIResponse.ok) {
+      throw new Error('Rate limited or unable to fetch location');
+    }
+
+    const locationResponseText = await locationFromAPIResponse.json();
+
+    if (locationResponseText) {
+      return JSON.stringify(locationResponseText);
+    }
+  } catch (error) {
+    console.error('Error fetching location:', error);
+  }
+
   return JSON.stringify({
     cityname: "Bengaluru",
   });
 };
+
+export const tools = [
+  {
+      type: "function",
+      function: {
+          name: "getCurrentWeather",
+          description: "Get the current weather",
+          parameters: {
+              type: "object",
+              properties: {
+                location: {
+                  type: "string",
+                  description: "location to get the weather for",
+                }
+              },
+              required: ["location"],
+              additionalProperties: false,
+          }
+      }
+  },
+  {
+      type: "function",
+      function: {
+          name: "getLocation",
+          description: "Get the user's current location",
+          parameters: {
+              type: "object",
+              properties: {}
+          }
+      }
+  },
+]
